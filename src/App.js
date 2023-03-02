a904dfc08cdeea1ed400305cffafc83ca7bd47dd
AleLatke <aleksalatinovic@gmail.com>
2023-03-02 19:02:45 +0100

    dopune sajta

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/navbar';
+import Card from './components/card';
+import podaci from './podaci';
 
 function App() {
+
+  let informacije = podaci.map(item => {
+    return(
+    <Card
+    key={item.id}
+    {...item}
+    />
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <NavBar/>
+    {informacije}
     </div>
   );
 }
---
+++ src/podaci.jsx
@@ -0,0 +1,103 @@
+export default [
+    {
+        id: 1,
+        naziv: "FRUŠKA GORA",
+        slika: require("./slike/Fruska-gora.jpg"),
+        opis: <h3>Fruška gora je ostrvska planina u Sremu.<br/>Najveći deo Fruške gore se nalazi u Srbiji (u Vojvodini), dok mali deo zalazi u istočnu Hrvatsku, u Vukovarsko-srijemsku županiju.<br/>
+        Prostire se na dužini od oko 75 km, širine je od 12 do 15 km i zahvata površinu od 525 km².<br/>
+        Najviši vrh je Crveni Čot (539m).<br/>
+        Fruška gora je ostrvska, stara gromadna planina. Doline i padine Fruške gore su prekrivene livadama, pašnjacima i žitnim poljima, na padinama su voćnjaci i vinogradi sa čuvenim vinskim podrumima, a delovi viši od 300 m su obrasli gustom listopadnom šumom.</h3>,
+        lokacija:"Srem,Srbija",
+        start:"1960",
+        kraj:"17.5.2022",
+        ocena:"7.9"
+    },
+    {
+        id: 2,
+        naziv: "ĐERDAP",
+        slika: require("./slike/djerdap3.jpg"),
+        opis:<h3>Nacionalni park Đerdap se nalazi u jugozapadnom delu Evrope, u severoistočnom delu Republike Srbije, na samoj međunarodnoj granici sa Rumunijom.<br/>
+        Ukupna površina Nacionalnog parka iznosi 93.968 hektara, a zaštitnom zonom obuhvaćeno je 63.608 hektara.<br/>
+        Osnovni prirodni fenomen ovog područja je magična Đerdapska klisura kroz koju protiče reka Dunav.<br/>
+        Flora Đerdapa se ne odlikuje samo raznovrsnošću i bogatstvom, nego i izrazitim verignim karakterom. Na prostoru parka opstaje preko 1100 biljnih vrsta.</h3>,
+        lokacija:"Golubac,Srbija",
+        start:"1974",
+        kraj:"25.5.2022",
+        ocena:"8.7"
+
+    },
+    {
+        id: 3,
+        naziv: "KOPAONIK",
+        slika: require("./slike/DubokaI1.jpg"),
+        opis:<h3>Копаоник (познат и као Сребрна планина) је највећи планински масив у јужној Србији који се
+         пружа од северозапада ка југоистоку дужином од око 75 km, досежући у средњем делу ширину од око 40 km.
+          Један његов део је заштићена зона под именом национални парк Копаоник у оквиру кога постоји већи број 
+          заштићених природних целина, а на њему се налази и највећи скијашки центар у Србији. Његов највиши врх 
+          је Панчићев врх са 2017 m нмв. на коме се налази маузолеј чувеног српског природњака по коме је добио име,
+           око кога се налази база војске Србије.</h3>,
+        lokacija:"Kopaonik,Srbija",
+        start:"1981",
+        kraj:"10.6.2022",
+        ocena:"9.1"
+
+    },
+    {
+        id: 4,
+        naziv: "TARA",
+        slika: require("./slike/tara.jpg"),
+        opis:<h3>Масив планине Таре налази се у западној Србији, у северозападном делу оивичен дубоким кањоном реке Дрине,
+        док му се огранци спуштају ка креманској долини и долини реке Ђетиње, где се ослања на огранке Златибора.
+        Подручје планине Таре сачињава најзападнију скупину из групе Старовлашко-рашких планина и, у ширем смислу,
+        састоји се од три подеоне целине, донекле издвојене речним долинама, превојима или седлима.</h3>,
+        lokacija:"Tara,Srbija",
+        start:"1981",
+        kraj:"10.6.2022",
+        ocena:"8.5"
+
+    },
+    {
+        id: 5,
+        naziv: "ŠAR PLANINA",
+        slika: require("./slike/sar-planina.jpg"),
+        opis:<h3>По својим основним морфотектонским особинама, она припада Шарско-пиндском планинском систему, односно Динаридима.
+        Шар-планину карактерише пространо било, чија дужина по хоризонталној пројекцији износи 80 до 85 km, односно територијално 
+        од Качаничке клисуре на североистоку, са врхом Љуботен, па све до горњег тока Радике и Врутока на југу,
+        територијално тромеђе македонско-албанско-српске границе.</h3>,
+        lokacija:"Prizren,Srbija",
+        start:"1986",
+        kraj:"10.6.2022",
+        ocena:"8.0"
+
+    },
+    {
+        id: 6,
+        naziv: "KUČAJ-BELJANICA",
+        slika: require("./slike/kucaj.jpg"),
+        opis:<h3>Парк природе „Кучај-Бељаница“ заузима велико пространство у Источној Србији. 
+        На северу је Жагубичком котлином одвојен од Хомољског масива, са запада га ограничава долина Мораве,
+        на југу је превојем Честобродица одвојен од масива Ртња, а на истоку је опасан Зајечарско-Борском депресијом.
+        Кучајско-бељанички масив је најпространији планински комплекс источне Србије.</h3>,
+        lokacija:"Despotovac,Srbija",
+        start:"2022",
+        kraj:"10.6.2022",
+        ocena:"7.8"
+
+    },
+    {
+        id: 7,
+        naziv: "STARA PLANINA",
+        slika: require("./slike/stara_planina.jpg"),
+        opis:<h3>Масив Старе планине налази се у источном појасу Карпато-балканида, у оквиру кога представља западни део масива Балкана,
+        који се у целини налази у Бугарској. Стару планину изграђује планински венац који почиње од обронака Вршке Чуке изнад Зајечара,
+        а затим се пружа према југоистоку, одакле се нагло издиже изнад Књажевца и прелази у високи планински масив који се лучно
+        пружа правцем северозапад-југоисток, достижући највећу висину на врху Миџор.</h3>,
+        lokacija:"Pirot,Srbija",
+        start:"2022",
+        kraj:"10.6.2022",
+        ocena:"8.9"
+
+    }
+
+
+]
